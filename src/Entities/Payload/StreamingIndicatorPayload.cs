82abb254d616bbc332c09ce266f9cef78bc3ba5f
Diego José Fernández Hernández <diego@diegofer.com>
2019-07-24 16:28:50 +0200

    feat: Added streaming indicator payload.

---
+++ src/Entities/Payload/StreamingIndicatorPayload.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using FoolishTech.Support.Binary;
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload
+{
+    sealed internal class StreamingIndicatorPayload 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal StreamingIndicator Indicator { get => ((StreamingIndicator)BinaryConverter.ReadUInt64(this.Storage.Slice(0, 8), BinaryConverter.Endianess.BigEndian)).DefinedOrDefault(); }
+
+        internal StreamingIndicatorPayload(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 8, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+
+            this.Storage = buffer.Slice(0, 8);
+        }
+    }
+
+    internal enum StreamingIndicator: UInt64
+    {
+        PlaybackWillOccurOnLocalDevice = 0x0,
+
+        PlaybackWillOccurOnAVAdapter = 0x5f9c8132b59f2fde,
+
+        PlaybackWillOccurOnAirPlay = 0xabb0256a31843974
+    }
+
+    internal static class StreamingIndicatorExtensions 
+    {
+        public static StreamingIndicator DefinedOrDefault(this StreamingIndicator state)
+        {   
+            return Enum.IsDefined(typeof(StreamingIndicator), state) ? state : StreamingIndicator.PlaybackWillOccurOnLocalDevice;
+        }
+    } 
+}
