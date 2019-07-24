5e6bf6cf36e5c63349b7a833a035d60b9a33aded
Diego José Fernández Hernández <diego@diegofer.com>
2019-07-24 16:25:29 +0200

    feat: Added Random values payloads for TLLV block

---
+++ src/Entities/Payload/R1Payload.cs
@@ -0,0 +1,20 @@
+using System;
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload
+{
+    sealed internal class R1Payload 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal byte[] R1 { get => this.Storage.Slice(0, 44).ToArray(); }
+
+        internal R1Payload(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 44, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+            
+            this.Storage = buffer.Slice(0, 44);
+        }
+    }
+}
---
+++ src/Entities/Payload/R2Payload.cs
@@ -0,0 +1,20 @@
+using System;
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload
+{
+    sealed internal class R2Payload 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal byte[] R2 { get => this.Storage.Slice(0, 21).ToArray(); }
+
+        internal R2Payload(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 21, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+            
+            this.Storage = buffer.Slice(0, 21);
+        }
+    }
+}
