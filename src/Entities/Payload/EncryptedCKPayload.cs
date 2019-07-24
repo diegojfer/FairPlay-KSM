350333e855933ea9c272163e936451345767cffc
Diego José Fernández Hernández <diego@diegofer.com>
2019-07-24 16:14:36 +0200

    feat: EncryptedCK included on project

---
+++ src/Entities/Payload/EncryptedCKPayload.cs
@@ -0,0 +1,21 @@
+using System;
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload
+{
+    sealed internal class EncryptedCKPayload 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal byte[] IV { get => this.Storage.Slice(0, 16).ToArray(); }
+        internal byte[] Key { get => this.Storage.Slice(16, 16).ToArray(); }
+
+        internal EncryptedCKPayload(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 32, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+            
+            this.Storage = buffer.Slice(0, 32);
+        }
+    }
+}
