8b78b4d4df9d7bc17d5a006b1298cc85d05856a9
Diego José Fernández Hernández <diego@diegofer.com>
2019-07-26 13:05:17 +0200

    feat: Added parcels enctypted entities.

---
+++ src/Entities/Payload/Parcel/EncryptedCKParcel.cs
@@ -0,0 +1,21 @@
+using System;
+
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload.Parcel
+{
+    sealed internal class EncryptedCKParcel 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal byte[] Key { get => this.Storage.Slice(0, 16).ToArray(); }
+
+        internal EncryptedCKParcel(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 16, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+            
+            this.Storage = buffer.Slice(0, 16);
+        }
+    }
+}
---
+++ src/Entities/Payload/Parcel/SKR1Parcel.cs
@@ -0,0 +1,24 @@
+using System;
+
+using FoolishTech.Support.Throws;
+
+namespace FoolishTech.FairPlay.Entities.Payload.Parcel
+{
+    sealed internal class SKR1Parcel 
+    {
+        private ReadOnlyMemory<byte> Storage { get; set; }
+
+        internal byte[] SK { get => this.Storage.Slice(0, 16).ToArray(); }
+        internal byte[] HU { get => this.Storage.Slice(16, 20).ToArray(); }
+        internal byte[] R1 { get => this.Storage.Slice(36, 44).ToArray(); }
+        internal byte[] Integrity { get => this.Storage.Slice(80, 16).ToArray(); }
+
+        internal SKR1Parcel(ReadOnlyMemory<byte> buffer)
+        {
+            // ArgumentThrow.IfNull(() => buffer, "Invalid buffer length. The buffer must not be null.", nameof(buffer)); /* STRUCT CAN NOT BE NULL. */
+            ArgumentThrow.IfLengthNot(buffer, 96, $"Invalid buffer length. The buffer must contains the exact number of bytes to fill entity '{this.GetType().FullName}'.", nameof(buffer));
+            
+            this.Storage = buffer.Slice(0, 96);
+        }
+    }
+}
