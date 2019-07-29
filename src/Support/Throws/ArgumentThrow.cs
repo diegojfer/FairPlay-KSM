6aba140d2f6077e39c08dd9392ad00d90bd8f405
Diego José Fernández Hernández <diego@diegofer.com>
2019-07-29 19:05:36 +0200

    feat: Argument thrower methods.

--- src/Support/Throws/ArgumentThrow.cs
+++ src/Support/Throws/ArgumentThrow.cs
@@ -23,6 +23,14 @@ namespace FoolishTech.Support.Throws
             if (buffer.Length < size) throw new ArgumentException(message, paramName);
         }
         
+        public static void IfLackingBytes(byte[] buffer, int size, string message, string paramName)
+        {
+            if (buffer == null) throw new ArgumentNullException(message, paramName);
+            if (size < 0) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(size));
+            if (buffer.Length < 0) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(buffer.Length));
+            if (buffer.Length < size) throw new ArgumentException(message, paramName);
+        }
+        
         public static void IfTooMuchBytes(ReadOnlyMemory<byte> buffer, int size, string message, string paramName)
         {
             if (size < 0) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(size));
@@ -47,6 +55,14 @@ namespace FoolishTech.Support.Throws
         
         public static void IfLengthNotMultiple(ReadOnlyMemory<byte> buffer, int multiple, string message, string paramName)
         {
+            if (multiple < 1) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(multiple));
+            if (buffer.Length < 0) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(buffer.Length));
+            if (buffer.Length % multiple != 0) throw new ArgumentException(message, paramName);
+        }
+        
+        public static void IfLengthNotMultiple(byte[] buffer, int multiple, string message, string paramName)
+        {
+            if (buffer == null) throw new ArgumentNullException(message, paramName);
             if (multiple < 1) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(multiple));
             if (buffer.Length < 0) throw new ArgumentOutOfRangeException("Invalid size length. ¿Integer overflow?", nameof(buffer.Length));
             if (buffer.Length % multiple != 0) throw new ArgumentException(message, paramName);
