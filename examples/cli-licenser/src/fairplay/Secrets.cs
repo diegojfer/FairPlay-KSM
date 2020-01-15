d7964c5cc5b917a556930c86519b61f7ce63265f
Diego Fernandez <diego@diegofer.com>
2020-01-15 02:01:10 +0100

    feat: Added cli-licenser.

---
+++ examples/cli-licenser/src/fairplay/Secrets.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace FoolishTech.FairPlay.CLILicenser
+{
+    public class Secrets
+    {
+        private static Secrets _instance = new Secrets();
+        public static Secrets Instance { get => _instance; } 
+
+        private Secrets() { }
+
+        // DON'T STORE SECRET HERE, 
+        // instead pass them as ENV. 
+        public byte[] CertificateBytes { get => Convert.FromBase64String(""); }
+        public string CertificatePassphrase { get => ""; }
+        public byte[] ASK { get => Convert.FromBase64String(""); }
+    }
+}
